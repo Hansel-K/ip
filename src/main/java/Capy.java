5b8ac7552cd21ac3344cad7820b931c9cc2a79b3
Hansel-K <e1122886@u.nus.edu>
2025-02-11 14:55:41 +0800

    Level 2. Add, List

--- src/main/java/Capy.java
+++ src/main/java/Capy.java
@@ -10,6 +10,9 @@ public class Capy {
         Scanner inputObj = new Scanner(System.in); // Create Scanner object
         String userInput; // Declare string to hold user input
 
+        String[] strArray = new String[100]; // Declare string array to store user inputs
+        int counter = 0; // Declare int to track how many strings have been added
+
         // Read input until "bye" is received
         while (true) {
 
@@ -20,9 +23,22 @@ public class Capy {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break; // Break loop when "bye" is received
-            } else {
+            } else if (userInput.equals("list")) {
+                // Print stored strings as a list
                 System.out.println("____________________________________________________________");
-                System.out.println(userInput); // Echo user input
+                for (int i = 0; i < counter; i++) {
+                    // Iterate through string array to print stored strings
+                    System.out.println(i + 1 +". " + strArray[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                // Add user input to string array
+                strArray[counter] = userInput;
+                counter++; // Update counter
+
+                // Echo that user input has been added
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + userInput);
                 System.out.println("____________________________________________________________");
             }
         }
